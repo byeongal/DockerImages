4016a16d99a6ce74e6bbbf97a0cd21cef336f968
YoungJae Kim <byeongal@comcom.ai>
2022-09-15 11:57:41 +0900

    pytorch + diffusers

--- pytorch-diffusers/start.sh
+++ pytorch-diffusers/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "SSH Server Start"
+sudo service ssh restart
+
+echo "Jupyter Lab Start"
 jupyter lab --NotebookApp.token="${PASSWORD}" --ip=0.0.0.0 &
 
-sudo /usr/sbin/sshd -D -o ListenAddress=0.0.0.0 &
+tail -f /dev/null
